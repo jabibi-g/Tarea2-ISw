113e5372ec802bd583d772e5482bce2636af6e21
Javier González <javier.gonzalez2101@alumnos.ubiobio.cl>
2024-09-05 19:48:58 -0400

    ADD Ejercicio2.js

---
+++ Ejercicio2.js
@@ -0,0 +1,22 @@
+//Ejercicio 2: Operadores Aritméticos.
+console.log("Ejercicio 2:");
+
+//Dados los siguientes valores numéricos:
+const numero1 = 20;
+const numero2 = 40;
+console.log(`Número 1: ${numero1}, Número 2: ${numero2}`);
+
+//Suma:
+console.log("Suma: ", numero1 + numero2);
+
+//Resta:
+console.log("Resta: ", numero1 - numero2);
+
+//Multiplicación:
+console.log("Multiplicación: ", numero1 * numero2);
+
+//División:
+console.log("División: ", numero1 / numero2);
+
+//Modulo:
+console.log("Modulo: ", numero1 % numero2);
