e687848115f5429a7b7c31bd4fd709765c43462b
Javier González <javier.gonzalez2101@alumnos.ubiobio.cl>
2024-09-05 19:48:28 -0400

    ADD Ejercicio1.js

---
+++ Ejercicio1.js
@@ -0,0 +1,17 @@
+// Author: Javier González Catalán
+
+//Ejercicio 1: Variables y Tipos de Datos.
+console.log("Ejercicio 1:")
+
+let nombre = "Javier";
+const edad = 22; //Variables 'const' son inmutables en el futuro (sólo lectura).
+const puedoConducir = false;
+
+console.log("Impresión de variables: ");
+console.log(nombre);
+console.log(edad);
+console.log(puedoConducir);
+console.log("\nImpresión de tipos de datos: ");
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof puedoConducir);
